9b413809d1d91b0d2112401d29f910b132443b17
GabrielCanteiro <gabrielc896@gmail.com>
2023-07-02 17:59:37 +0100

    entrega desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Smartphone iphoneGabriel = new Iphone("910474328","Iphone 12","123123kkk12asdg12",128);
+iphoneGabriel.InstalarAplicativo("WhatsApp");
+iphoneGabriel.Ligar();
+iphoneGabriel.ReceberLigacao();
+
+
+Console.WriteLine("\n");
+
+
+Smartphone nokiaGabriel = new Nokia("910471771","Nokia tijolao","11223344HHHH22",64);
+nokiaGabriel.InstalarAplicativo("Telegram");
+nokiaGabriel.Ligar();
+nokiaGabriel.ReceberLigacao();
